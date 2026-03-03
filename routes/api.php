d60f7df4571a5cd30cc587c2c735287322343faa
aurelioo29 <aureliolucio5@gmail.com>
2026-03-03 19:16:48 +0700

    fix: bug routing

--- routes/api.php
+++ routes/api.php
@@ -39,14 +39,13 @@ Route::get('/ping', function () {
     ]);
 });
 
-Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
     // Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+    Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
     // email verification
     Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
